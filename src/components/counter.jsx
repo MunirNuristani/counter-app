a833f4cf895ec9a90b5dd3f90a1c7de89df17808
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-03-06 13:49:33 -0500

    Updating the state and handling event arguements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,8 @@ class Counter extends Component {
         count: 0
     };
 
-    handleIncrement = () => {
-        console.log("Increment Clicked", this.state.count);
+    handleIncrement = product => {
+        this.setState({ count: this.state.count + 1 });
     }
 
     render() { 
@@ -14,7 +14,7 @@ class Counter extends Component {
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button 
-                onClick= {this.handleIncrement} 
+                onClick= { () => this.handleIncrement(product)} 
                 className="btn btn-secondary btn-sm">
                     Increment
             </button>
