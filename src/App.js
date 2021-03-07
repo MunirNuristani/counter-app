1846d338828efb3ad96309aa23da8869358b7d8a
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-03-06 22:42:21 -0500

    Lifecycle hooks, updated phase, stateless function components

--- src/App.js
+++ src/App.js
@@ -12,33 +12,43 @@ class App extends Component {
         {id: 3, value: 0},
         {id: 4, value: 0}
     ]
-}
+  }
 
-handleIncrement = counter => {
+  constructor() {
+    super();
+    console.log('App - Constructor');
+  }
+
+  componentDidMount() {
+    console.log("App - Mounted");
+  }
+
+  handleIncrement = counter => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = {...counter};
     counters[index].value++;
     this.setState({ counters });
-}
+  }
 
-handleReset = () => {
+  handleReset = () => {
     const counters = this.state.counters.map(c => {
         c.value = 0;
         return c;
     });
     this.setState({ counters });
-};
+  };
 
-handleDelete = (counterId) => {
+  handleDelete = (counterId) => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
-};
+  };
 
   render(){
+    console.log("App - Rendered" );
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
           <main className="container">
             <Counters 
             counters={this.state.counters}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    state = {  }
-    render() { 
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                </div>
-            </nav>
-          );
-    }
-}
- 
+// Stateless Function Component
+const NavBar = ({ totalCounters }) => {
+    console.log("NavBar - Rendered");
+    
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Navbar 
+                    <span className="badge badge-pill badge-secondary">
+                        {totalCounters}
+                    </span>
+                </a>
+            </div>
+        </nav>
+    );
+};
+
 export default NavBar;
